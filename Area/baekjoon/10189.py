3b2385c583e3e0bcd54d6ffa63c952e34da9b247
전상언 <131582356+RobertJeon@users.noreply.github.com>
2024-02-21 20:31:05 +0900

    240221_add_baekjoon

---
+++ Area/baekjoon/10189.py
@@ -0,0 +1,16 @@
+"""
+문제
+Print out the word Hook as shown below.
+
+출력
+Print out the word Hook as shown below.
+"""
+def hook():
+    return """#  # #### #### #  #
+            #### #  # #  # # #
+            #### #  # #  # # #
+            #  # #### #### #  #"""
+
+
+if __name__ == "__main__":
+    print(hook())
